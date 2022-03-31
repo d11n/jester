39d5e282de68629cc8e5224128786a8aff5e3951
Derek <derek.dunagan@gmail.com>
2022-03-31 13:38:36 -0400

    simplify

--- apps/guest-graphql/schema/ping/ping.resolvers.js
+++
@@ -1,9 +0,0 @@
-export const resolvers = {
-	Query: {
-		ping: (_, { message }) => {
-			const return_m = message || "Pong";
-
-			return { message: return_m };
-		},
-	},
-};
--- server.test.js
+++ server.test.js
@@ -1,31 +1,6 @@
-import { test, expect } from "@jest/globals"
-
-import Server from "./server.js";
-
-/////////// Tests
-
-test(".serve", () => {
-	const server = new Server(Schema);
-
-	expect.assertions(4)
-	expect(() => server.serve()).toThrow()
-	expect(() => server.serve("nonsense")).toThrow()
-	expect(() => server.serve(random())).not.toThrow()
-	expect(() => server.close()).not.toThrow()
+describe("Thing", () => {
+	test("it should do a thing", () => {
+		const i = true;
+		expect(i).toEqual(false);
+	})
 });
-
-test(".close", () => {
-	const server = new Server(Schema);
-
-	expect.assertions(2)
-	expect(() => server.serve(random())).not.toThrow()
-	expect(() => server.close()).not.toThrow()
-});
-
-test.todo(".execute");
-
-/////////// Util
-
-function random(min=1000, max=4000) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-}
