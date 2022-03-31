043106b267e3d3d581b5c3df39d6bc900dd87a5c
Derek <derek.dunagan@gmail.com>
2022-03-31 13:43:30 -0400

    max simplicity

--- server.test.js
+++ server.test.js
@@ -1,6 +1,5 @@
-describe("Thing", () => {
-	test("it should do a thing", () => {
-		const i = true;
-		expect(i).toEqual(false);
-	})
-});
+import { test, expect } from "@jest/globals"
+
+test("What is the meaning of life, the universe, and everything?", () => {
+	expect(42).toEqual(42)
+})
